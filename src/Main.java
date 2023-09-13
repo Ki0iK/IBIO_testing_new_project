8c1420c2c7b82976271874d8c9e0a36d72127cfc
Kio <kio.m@gmail.com>
2023-09-13 15:58:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import static ibio.Helpers.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int i = inputInt("Please type an int: "); // type in the output (with comp.)
+        output(i * i); // squaring
+    }
+}
